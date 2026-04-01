f8bc73edf00864beb9fa321735e66a84a1f7044c
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-04-01 23:39:13 +0200

    Created the main.h

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * _printf - produces output according to a format
+ * @format: format string
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...);
+
+#endif /* MAIN_H */
